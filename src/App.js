6b0fa05b8b0a998310a7b3f28b0ab81238402149
Yoko <40120kyoko@gmail.com>
2020-07-08 03:44:13 +0700

    belajar routing
    hehe mari

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+
 import "./App.css";
 
-import HomePage from "./homepage.component";
+import HomePage from "../src/pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    {/* <Link to="/topics">aaa</Link> */}
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+const TopicsList = () => (
+  <div>
+    <h1>TOPICS LIST</h1>
+  </div>
+);
+
+const TopicDetail = () => (
+  <div>
+    <h1>TOPIC DETAIL PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/topics" component={TopicsList} />
+        <Route path="/topics/:topicsId" component={TopicDetail} />
+      </Switch>
     </div>
   );
 }
