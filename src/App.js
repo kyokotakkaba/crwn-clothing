b4b8397e33cb1723d82d30b793bb8239924d49b8
Yoko <40120kyoko@gmail.com>
2020-07-08 10:56:45 +0700

    test test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
-
-import "./App.css";
-
-import HomePage from "../src/pages/homepage/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 const HatsPage = () => (
   <div>
