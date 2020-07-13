7e7d10d09c2c99a3c31fe095a5b157a931a11f2d
kyokotakkaba <40120kyoko@gmail.com>
2020-07-13 11:29:40 +0700

    3. Collection Item [done]

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+
+import { Switch, Route } from "react-router-dom";
+
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    {/* <Link to="/topics">aaa</Link> */}
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
-const TopicsList = () => (
-  <div>
-    <h1>TOPICS LIST</h1>
-  </div>
-);
-
-const TopicDetail = () => (
-  <div>
-    <h1>TOPIC DETAIL PAGE</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/topics" component={TopicsList} />
-        <Route path="/topics/:topicsId" component={TopicDetail} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
